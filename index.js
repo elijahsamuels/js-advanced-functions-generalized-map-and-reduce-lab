37cdf6a0e109464fb7b8705924fba36ed05c17ac
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-02 11:05:31 -0700

    commit tree 3a380596da849e247c18ee3b586b4095bc6a8a91

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+
+const src = [1, 2, 3, -9]
+
+function map(src, callback) {
+	let newArray = [];
+	 for (let i = 0; i < src.length; i++) {
+		 newArray.push(callback(src[i]))
+	 }
+	 return newArray;
+ }
+
+
+function reduce(array, callback, startingPoint) {
+    let memo;
+    let i = 0;
+    if (startingPoint) { 
+        memo = startingPoint 
+    } else {
+        memo = array[0]
+        i = 1
+    }
+
+    for (i; i < array.length; i++) {
+        memo = callback(array[i], memo)
+    } 
+    return memo;
+}
+
